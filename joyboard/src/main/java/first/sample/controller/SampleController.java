761ba45ca3f42182cc562350268c0dcda5be4a96
heesunjeong <jhswit@naver.com>
2017-05-30 00:59:06 +0900

    HandlerMethodArgumentResolver 적용

--- joyboard/src/main/java/first/sample/controller/SampleController.java
+++ joyboard/src/main/java/first/sample/controller/SampleController.java
@@ -1,21 +1,49 @@
 package first.sample.controller;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.common.common.CommandMap;
+
 @Controller
 public class SampleController {
 	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="sampleService")
+	private SampleService sampleService;
 
 	@RequestMapping(value = "/sample/openSampleList.do")
 	public ModelAndView openSampleList(Map<String, Object> commandMap) throws Exception {
-		ModelAndView mv = new ModelAndView("");
+		ModelAndView mv = new ModelAndView("BoardList");
 		log.debug("인터셉터 테스트");
+		
+		List<Map<String, Object>> list = sampleService.selectBoardList(commandMap);
+		mv.addObject("list", list);
 
 		return mv;
+	}
+	
+	@RequestMapping(value="/sample/testMapArgumentResolver.do")
+	public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception{
+	    ModelAndView mv = new ModelAndView("");
+	     
+	    if(commandMap.isEmpty() == false){
+	        Iterator<Entry<String,Object>> iterator = commandMap.getMap().entrySet().iterator();
+	        Entry<String,Object> entry = null;
+	        while(iterator.hasNext()){
+	            entry = iterator.next();
+	            log.debug("key : "+entry.getKey()+", value : "+entry.getValue());
+	        }
+	    }
+	    return mv;
 	}
 }
